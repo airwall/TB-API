2a4ff106c9372f4f5b4412d24d35db2aaadb3622
Belinschi Dima <belinski.d@gmail.com>
2016-08-03 18:02:39 +0300

    add method .save_data_to_db

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,29 +1,27 @@
 class Course < ApplicationRecord
-
   def self.fetch
-    response = RestClient.post 'http://s1.teachbase.ru/oauth/token', {
-      grant_type: 'client_credentials',
-      client_id: Rails.application.secrets.client_id,
-      client_secret: Rails.application.secrets.client_secret
-    }
+    response = RestClient.post "http://s1.teachbase.ru/oauth/token", grant_type: "client_credentials",
+                                                                     client_id: Rails.application.secrets.client_id,
+                                                                     client_secret: Rails.application.secrets.client_secret
 
     token = JSON.parse(response)["access_token"]
-    content = JSON.parse(RestClient.get 'http://s1.teachbase.ru/endpoint/v1/course_sessions', { 'Authorization' => "Bearer #{token}" })
+    content = JSON.parse(RestClient.get("http://s1.teachbase.ru/endpoint/v1/course_sessions", "Authorization" => "Bearer #{token}"))
+    # self.save_data_to_db(content)
+  end
 
+  def self.save_data_to_db(content)
     content.each do |c|
-      if Course.exists?(course_id: c["course"]["id"]) == false && c["access_type"] == "open"
-        Course.create(session_name: c["name"],
-                      started_at:   c["started_at"],
-                      access_type:  c["access_type"],
-                      finished_at:  c["finished_at"],
-                      apply_url:    c["apply_url"],
-                      course_id:    c["course"]["id"],
-                      course_name:  c["course"]["name"],
-                      owner_name:   c["course"]["owner_name"],
-                      cower_url:    c["course"]["cower_url"],
-                      description:  c["course"]["description"],
-                      )
-      end
+      next unless exists?(course_id: c["course"]["id"]) == false && c["access_type"] == "open"
+      create(session_name: c["name"],
+             started_at:   c["started_at"],
+             access_type:  c["access_type"],
+             finished_at:  c["finished_at"],
+             apply_url:    c["apply_url"],
+             course_id:    c["course"]["id"],
+             course_name:  c["course"]["name"],
+             owner_name:   c["course"]["owner_name"],
+             cower_url:    c["course"]["cower_url"],
+             description:  c["course"]["description"])
     end
   end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,9 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Course, type: :model do
-  describe ".fetch" do
-    it "should perform correct action on system data based on mocked response" do
-      Course.fetch
+  describe ".save_data_to_db" do
+    context "should save course from open session" do
+      it "Save" do
+        json = JSON.parse(File.read("#{Rails.root}/spec/support/shared/course.json"))
+        expect { Course.save_data_to_db(json) }.to change(Course, :count).by(1)
+        expect(Course.first.access_type).to eq "open"
+      end
+    end
+
+    context "should don't save course from open session if course exist" do
+      let!(:course) { create(:course) }
+      it "Don't save" do
+        json = JSON.parse(File.read("#{Rails.root}/spec/support/shared/course.json"))
+        expect { Course.save_data_to_db(json) }.to change(Course, :count).by(0)
+      end
     end
   end
 end
