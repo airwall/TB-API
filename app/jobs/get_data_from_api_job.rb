e79a6c3244eaf01b5deeba7bd9a0092a6f5affda
Belinschi Dima <belinski.d@gmail.com>
2016-08-16 21:12:57 +0300

    *

--- app/jobs/get_data_from_api_job.rb
+++ app/jobs/get_data_from_api_job.rb
@@ -2,10 +2,10 @@ class GetDataFromApiJob < ApplicationJob
   queue_as :default
 
   def perform
-    token ||= JSON.parse(RestClient.post("http://s.teachbase.ru/oauth/token", grant_type: "client_credentials",
+    token ||= JSON.parse(RestClient.post("http://s1.teachbase.ru/oauth/token", grant_type: "client_credentials",
                                                                               client_id: Rails.application.secrets.client_id,
                                                                               client_secret: Rails.application.secrets.client_secret))["access_token"]
-    content = JSON.parse(RestClient.get("http://s.teachbase.ru/endpoint/v1/course_sessions", "Authorization" => "Bearer #{token}"))
+    content = JSON.parse(RestClient.get("http://s1.teachbase.ru/endpoint/v1/course_sessions", "Authorization" => "Bearer #{token}"))
 
     content.each do |c|
       next unless c["access_type"] == "open"
